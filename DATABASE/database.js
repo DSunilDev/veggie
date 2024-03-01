a61989dfcfe4eef58c900fadee4b053e8abc9a78
DSunilDev <sunilkumardeveops@gmail.com>
2024-03-01 18:19:58 +0530

    o

--- DATABASE/database.js
+++ DATABASE/database.js
@@ -6,7 +6,7 @@ const MongoClient=mongodb.MongoClient;
 let database;
 async function connect()
 {
-  const client=await MongoClient.connect('');
+  const client=await MongoClient.connect('mongodb://127.0.0.1:27017');
   database=client.db('abr')
 }
 
